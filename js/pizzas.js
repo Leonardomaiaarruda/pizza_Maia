cab74e05aa8f6da4de91a74f489eca3f5c7ba8ea
Leonardo Maia Arruda <leonardomaiaarruda@gmail.com>
2022-09-11 17:16:38 -0300

    Atualização das imagens

--- js/pizzas.js
+++ js/pizzas.js
@@ -1,5 +1,5 @@
   export  let pizzaJson = [
-        {id:1, name:'Mussarela', img:'images/mussarela.png', price:[10.0, 20.0, 30.0] ,sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+        {id:1, name:'Mussarela', img:'images/Mussarela.png', price:[10.0, 20.0, 30.0] ,sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
         {id:2, name:'Moda da casa', img:'images/moda-da-casa.png', price:[100.0, 200.0, 330.0], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
         {id:3, name:'Quatro Queijos', img:'images/quatro-queijos.png', price:[1000.0, 2000.0, 3000.0], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
         {id:5, name:'california', img:'images/california.png', price:[10.0, 20.0, 30.0], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
